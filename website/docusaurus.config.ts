263f6354b113807e3416b49f05c82750e2956681
Divyanshu Bhargava <divyanshub024@gmail.com>
2025-01-03 18:13:32 +0400

    fix: Fix the config file

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -12,14 +12,14 @@ const config: Config = {
   url: 'https://buildMirai.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'buildMirai', // Usually your GitHub org/user name.
   projectName: 'mirai', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
