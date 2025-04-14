5ca4bb72a8fa888eb797e1d78f80f2b4b69221a4
Odinachi David <odinachidavid@yahoo.com>
2025-04-14 16:25:20 +0100

    update

--- packages/stac/lib/src/parsers/stac_dropdown_menu/stac_dropdown_menu.dart
+++ packages/stac/lib/src/parsers/stac_dropdown_menu/stac_dropdown_menu.dart
@@ -9,35 +9,38 @@ import 'package:stac/src/utils/text_input_utils.dart';
 
 export 'package:stac/src/parsers/stac_container/stac_container_parser.dart';
 
-part 'stac_dropdown_menu.freezed.dart';part 'stac_dropdown_menu.g.dart';
+part 'stac_dropdown_menu.freezed.dart';
+
+part 'stac_dropdown_menu.g.dart';
 
 @freezed
 abstract class StacDropdownMenu with _$StacDropdownMenu {
-  const factory StacDropdownMenu(
-      {@Default(true) bool enabled,
-      double? width,
-      double? menuHeight,
-      Map<String, dynamic>? leadingIcon,
-      Map<String, dynamic>? trailingIcon,
-      Map<String, dynamic>? label,
-      String? hintText,
-      String? helperText,
-      String? errorText,
-      Map<String, dynamic>? selectedTrailingIcon,
-      bool? enableFilter,
-      bool? enableSearch,
-      StacTextInputType? keyboardType,
-      StacTextStyle? textStyle,
-      @Default(TextAlign.start) TextAlign textAlign,
-      StacInputDecorationTheme? inputDecorationTheme,
-      @Default([]) List<StacInputFormatter> inputFormatters,
-      StacOffset? alignmentOffset,
-      StacEdgeInsets? expandedInsets,
-      bool? requestFocusOnTap,
-      dynamic initialSelection,
-      @Default([]) List<StacDropdownMenuEntry> dropdownMenuEntries,
-      @Default(DropdownMenuCloseBehavior.all)
-      DropdownMenuCloseBehavior? closeBehavior}) = _StacDropdownMenu;
+  const factory StacDropdownMenu({
+    @Default(true) bool enabled,
+    double? width,
+    double? menuHeight,
+    Map<String, dynamic>? leadingIcon,
+    Map<String, dynamic>? trailingIcon,
+    Map<String, dynamic>? label,
+    String? hintText,
+    String? helperText,
+    String? errorText,
+    Map<String, dynamic>? selectedTrailingIcon,
+    bool? enableFilter,
+    bool? enableSearch,
+    StacTextInputType? keyboardType,
+    StacTextStyle? textStyle,
+    @Default(TextAlign.start) TextAlign textAlign,
+    StacInputDecorationTheme? inputDecorationTheme,
+    @Default([]) List<StacInputFormatter> inputFormatters,
+    StacOffset? alignmentOffset,
+    StacEdgeInsets? expandedInsets,
+    bool? requestFocusOnTap,
+    dynamic initialSelection,
+    @Default([]) List<StacDropdownMenuEntry> dropdownMenuEntries,
+    @Default(DropdownMenuCloseBehavior.all)
+    DropdownMenuCloseBehavior? closeBehavior,
+  }) = _StacDropdownMenu;
 
   factory StacDropdownMenu.fromJson(Map<String, dynamic> json) =>
       _$StacDropdownMenuFromJson(json);
