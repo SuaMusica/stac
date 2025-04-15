8bc181717031be041ea64b8566dcabd45b38fbc6
Divyanshu Bhargava <divyanshub024@gmail.com>
2025-04-15 23:49:06 +0530

    Update import path for action parsers to match new directory structure

--- packages/stac/lib/stac.dart
+++ packages/stac/lib/stac.dart
@@ -1,4 +1,4 @@
-export 'package:stac/src/action_parsers/action_parsers.dart';
+export 'package:stac/src/parsers/actions/actions.dart';
 export 'package:stac/src/framework/framework.dart';
 export 'package:stac/src/parsers/parsers.dart';
 export 'package:stac/src/services/services.dart';
