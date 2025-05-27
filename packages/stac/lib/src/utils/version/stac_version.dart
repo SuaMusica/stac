abb666c4a0b50691bd0954e8791d13543548c84e
Felipe Sales <soufeliposales@gmail.com>
2025-05-27 14:52:36 -0300

    test: adjust test and behaviours

--- packages/stac/lib/src/utils/version/stac_version.dart
+++ packages/stac/lib/src/utils/version/stac_version.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:stac/stac.dart';
 
 class StacVersion {
@@ -16,15 +14,13 @@ class StacVersion {
   factory StacVersion.fromJson(Map<String, dynamic> json) {
     return StacVersion(
       buildNumber: toPlatformJson(json, 'buildNumber').toInt(),
-      condition: toPlatformJson(json, 'condition').toStacConditionVersion(),
+      condition: (toPlatformJson(json, 'condition') as String?)
+          .toStacConditionVersion(),
     );
   }
 
   static dynamic toPlatformJson(Map<String, dynamic> json, String key) =>
       json['${key}_${Platform.operatingSystem}'] ?? json[key];
-
-  static StacConditionVersion fromJsonCondition(String? json) =>
-      json?.toStacConditionVersion() ?? StacConditionVersion.notEqual;
 }
 
 enum StacConditionVersion {
@@ -43,6 +39,7 @@ extension StacConditionVersionX on String? {
         '<' => StacConditionVersion.lessThan,
         '<=' => StacConditionVersion.lessThanOrEqual,
         '==' => StacConditionVersion.equal,
+        '!=' => StacConditionVersion.notEqual,
         _ => StacConditionVersion.notEqual,
       };
 }
@@ -64,49 +61,18 @@ extension ListStringX on List<String> {
 
 extension StacVersionX on StacVersion {
   bool isSatisfied() {
-    final buildNumber = StacRegistry.instance.buildNumber;
-
-    if (buildNumber == null) {
-      return false;
-    }
-
-    debugPrint(
-        'stacteste: buildNumber: $buildNumber | buildNumber: $buildNumber');
-
-    final current = buildNumber.split('.').toIntList();
-    final target = buildNumber.split('.').toIntList();
-
-    final maxLength =
-        [current.length, target.length].reduce((a, b) => a > b ? a : b);
-
-    while (current.length < maxLength) {
-      current.add(0);
-    }
-    while (target.length < maxLength) {
-      target.add(0);
-    }
-
-    for (int i = 0; i < maxLength; i++) {
-      if (current[i] != target[i]) {
-        final comp = current[i].compareTo(target[i]);
-        return switch (condition) {
-          StacConditionVersion.greaterThan => comp > 0,
-          StacConditionVersion.greaterThanOrEqual => comp >= 0,
-          StacConditionVersion.lessThan => comp < 0,
-          StacConditionVersion.lessThanOrEqual => comp <= 0,
-          StacConditionVersion.equal => false,
-          StacConditionVersion.notEqual => true,
-        };
-      }
+    final appBuildNumber = StacRegistry.instance.buildNumber;
+    if (appBuildNumber == null) {
+      return true;
     }
 
     return switch (condition) {
-      StacConditionVersion.greaterThan => false,
-      StacConditionVersion.greaterThanOrEqual => true,
-      StacConditionVersion.lessThan => false,
-      StacConditionVersion.lessThanOrEqual => true,
-      StacConditionVersion.equal => true,
-      StacConditionVersion.notEqual => false,
+      StacConditionVersion.greaterThan => appBuildNumber > buildNumber,
+      StacConditionVersion.greaterThanOrEqual => appBuildNumber >= buildNumber,
+      StacConditionVersion.lessThan => appBuildNumber < buildNumber,
+      StacConditionVersion.lessThanOrEqual => appBuildNumber <= buildNumber,
+      StacConditionVersion.equal => appBuildNumber == buildNumber,
+      StacConditionVersion.notEqual => appBuildNumber != buildNumber,
     };
   }
 }
