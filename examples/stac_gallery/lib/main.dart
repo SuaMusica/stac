dac07479020be2d832c44e556bd7d339b0553fcc
Divyanshu Bhargava <divyanshub024@gmail.com>
2025-07-26 11:22:44 +0530

    refactor: simplify StacGallery layout and update StacAppBarParser for optional systemOverlayStyle

--- examples/stac_gallery/lib/main.dart
+++ examples/stac_gallery/lib/main.dart
@@ -4,12 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:stac/stac.dart' show Stac;
 import 'package:stac_gallery/app/example/example_screen_parser.dart';
 import 'package:stac_models/core/stac_model.dart';
-import 'package:stac_models/painting/stac_box_decoration/stac_box_decoration.dart';
 import 'package:stac_models/painting/stac_color/stac_colors.dart';
-import 'package:stac_models/painting/stac_text_style/stac_text_style.dart';
-import 'package:stac_models/types/stac_border/stac_border.dart';
 import 'package:stac_models/types/stac_double.dart';
-import 'package:stac_models/types/stac_text_types.dart';
 import 'package:stac_models/widgets/widgets.dart';
 import 'package:stac_webview/stac_webview.dart';
 
@@ -39,8 +35,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('json: ${homeScreen().toJson()}');
-
     return MaterialApp(
       title: 'STAC Gallery',
       theme: ThemeData(
@@ -55,28 +49,14 @@ StacWidget homeScreen() {
   return StacScaffold(
     appBar: StacAppBar(
       title: StacText(
-        data: 'Hello, Stac!!',
+        data: 'STAC Gallery',
       ),
     ),
     body: StacCenter(
       child: StacContainer(
-        width: StacDouble(200),
-        height: StacDouble(200),
-        decoration: StacBoxDecoration(
-          color: StacColors.red,
-          border: StacBorder(
-            color: StacColors.black,
-            width: 10,
-          ),
-        ),
-        child: StacText(
-          data: 'Hello, Stac!!',
-          style: StacTextStyle(
-            color: StacColors.white,
-            fontSize: 20,
-            fontWeight: StacFontWeight.bold,
-          ),
-        ),
+        color: StacColors.blue,
+        width: StacDouble(100.0),
+        height: StacDouble(100.0),
       ),
     ),
   );
