81d0096787b8fd371ce174e23db1aa156203b24f
Divyanshu Bhargava <divyanshub024@gmail.com>
2025-08-01 00:42:35 +0530

    Refactor: StacElevated Button

--- packages/stac/lib/src/parsers/widgets/stac_elevated_button/stac_elevated_button_parser.dart
+++ packages/stac/lib/src/parsers/widgets/stac_elevated_button/stac_elevated_button_parser.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:stac/src/framework/framework.dart';
+import 'package:stac/src/parsers/core/stac_widget_parser.dart';
 import 'package:stac/src/parsers/theme/stac_button_style/stac_button_style.dart';
+import 'package:stac/src/parsers/types/type_parser.dart';
 import 'package:stac/src/utils/widget_type.dart';
 import 'package:stac_framework/stac_framework.dart';
-
-import 'stac_elevated_button.dart';
+import 'package:stac_models/widgets/elevated_button/stac_elevated_button.dart';
 
 class StacElevatedButtonParser extends StacParser<StacElevatedButton> {
   const StacElevatedButtonParser();
@@ -30,8 +31,8 @@ class StacElevatedButtonParser extends StacParser<StacElevatedButton> {
           value == false ? null : model.onFocusChange,
       style: model.style?.parseElevatedButton(context),
       autofocus: model.autofocus,
-      clipBehavior: model.clipBehavior,
-      child: Stac.fromJson(model.child, context),
+      clipBehavior: model.clipBehavior.parse,
+      child: model.child?.parse(context),
     );
   }
 }
--- packages/stac_models/lib/types/types.dart
+++ packages/stac_models/lib/types/types.dart
@@ -1,5 +1,12 @@
 library;
 
 export 'stac_alignment_directional.dart';
+export 'stac_border_radius/stac_border_radius.dart';
+export 'stac_border_side/stac_border_side.dart';
 export 'stac_drag_start_behavior.dart';
+export 'stac_duration/stac_duration.dart';
 export 'stac_floating_action_button_location.dart';
+export 'stac_mouse_cursor.dart';
+export 'stac_rounded_rectangle_border/stac_rounded_rectangle_border.dart';
+export 'stac_size/stac_size.dart';
+export 'stac_visual_density/stac_visual_density.dart';
