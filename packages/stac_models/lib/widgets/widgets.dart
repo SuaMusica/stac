e758d2a3ea9a737b6941c982650f642a61fc0559
Mithul Nayagam <mitthulnayagam@gmail.com>
2025-08-07 21:39:26 +0530

    added StacDynamicView to DSL

--- packages/stac_models/lib/widgets/widgets.dart
+++ packages/stac_models/lib/widgets/widgets.dart
@@ -5,6 +5,7 @@ export 'center/stac_center.dart';
 export 'column/stac_column.dart';
 export 'container/stac_container.dart';
 export 'divider/stac_divider.dart';
+export 'dynamic_view/stac_dynamic_view.dart';
 export 'elevated_button/stac_elevated_button.dart';
 export 'expanded/stac_expanded.dart';
 export 'filled_button/stac_filled_button.dart';
