6e8631de9ff0b68393c1d761e60073ce55e33928
Mithul Nayagam <mitthulnayagam@gmail.com>
2025-08-15 14:55:12 +0530

    added StacDrawer to DSL

--- packages/stac_models/lib/widgets/widgets.dart
+++ packages/stac_models/lib/widgets/widgets.dart
@@ -11,6 +11,7 @@ export 'colored_box/stac_colored_box.dart';
 export 'clip_rrect/stac_clip_rrect.dart';
 export 'clip_oval/stac_clip_oval.dart';
 export 'circular_progress_indicator/stac_circular_progress_indicator.dart';
+export 'drawer/stac_drawer.dart';
 export 'chip/stac_chip.dart';
 export 'column/stac_column.dart';
 export 'container/stac_container.dart';
