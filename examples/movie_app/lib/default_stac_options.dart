2b5ece5cbe73d4ec2ae27c750b011930687f61d0
Divyanshu Bhargava <divyanshub024@gmail.com>
2025-09-22 17:18:44 +0530

    Remove unused files: default_stac_options.dart and hello_world.dart from the movie_app example

--- examples/movie_app/lib/default_stac_options.dart
+++
@@ -1,9 +0,0 @@
-import 'package:stac_core/core/stac_options.dart';
-
-class DefaultStacOptions {
-  static const StacOptions options = StacOptions(
-    name: 'movie-app',
-    description: '',
-    projectId: 'g9W07DpZKtDjjZvxU6Rr',
-  );
-}
